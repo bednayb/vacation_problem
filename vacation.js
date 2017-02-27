9b897aaea0d9362c4d6e8ad0561cafd3ba299add
bednayb <bednaybence@gmail.com>
2017-02-27 13:37:05 +0100

    add rules

--- vacation.js
+++ vacation.js
@@ -1,13 +1,26 @@
 'use strict';
 
-var array = ['x','y','z'];
-var rules1 = [['x','y'], ['y','z']];
+// Destinations
+const destinations = ['x','y','z','a','b'];
+
+// Destinations rules -- in small array contains the rules. the first should be before the second. Example: [['x','y'], ['y','z']]  x --> y  and y --> z
+const rules1 = [['y','x'], ['z','x'],['z','y'],['b','a']];
+const rules2 = [['y','x']];
 
 
 function vacation(plan, rules){
-  var result = plan;
-
-  return result
+  console.log(plan);
+  for (let i of rules) {
+    if(plan.indexOf(i[0]) > plan.indexOf(i[1])){
+       plan[plan.indexOf(i[0])] = i[1];
+       plan[plan.indexOf(i[1])] = i[0];
+      // console.log(plan);
+    }
+  }
+  console.log(plan);
+  return plan
 }
+
+vacation(destinations,rules1)
 
 module.exports = vacation;
