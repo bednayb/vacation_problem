70a65337b835ba3b17643d8dabbe685b3824ea45
bednayb <bednaybence@gmail.com>
2017-02-27 10:32:08 +0100

    add test file with 3 basic test

---
+++ test.js
@@ -0,0 +1,20 @@
+var test = require('tape')
+var vacation = require('./vacation.js')
+
+//TEST WORK
+test('true', function (t) {
+  t.equal(1, 1);
+  t.end();
+});
+
+//Vacation function WORKS
+test('x with no rules result x', function (t) {
+  t.deepEqual(vacation(['x'],[]),['x']);
+  t.end();
+})
+
+// Works with more vacation plan
+test('x with no rules result x', function (t) {
+  t.deepEqual(vacation(['x','y','z'],[]),['x','y','z']);
+  t.end();
+})
