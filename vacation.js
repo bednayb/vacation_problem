790080be9bb2351e1d8f900c46561217eac98562
bednayb <bednaybence@gmail.com>
2017-02-27 14:09:10 +0100

    add error message  if a destination exists more than one

--- vacation.js
+++ vacation.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Destinations
-const destinations = ['x','y','z','a','b'];
+const destinations = ['x','a','z','a','w'];
 
 // Destinations rules -- in small array contains the rules. the first should be before the second. Example: [['x','y'], ['y','z']]  x --> y  and y --> z
 const rules1 = [['y','x'], ['z','x'],['z','y'],['b','a']];
@@ -9,12 +9,19 @@ const rules2 = [['y','x']];
 
 
 function vacation(plan, rules){
-  console.log(plan);
+  //Check every destinations exists just once
+  let checkDestinationsOnce = plan.sort();
+  console.log('pince',checkDestinationsOnce);
+  for(let i = 0; i < checkDestinationsOnce.length-1; i++){
+    if(checkDestinationsOnce[i] === checkDestinationsOnce[i+1]){
+      return "Use every destinations just once"
+    }
+  }
+  //check rules
   for (let i of rules) {
     if(plan.indexOf(i[0]) > plan.indexOf(i[1])){
        plan[plan.indexOf(i[0])] = i[1];
        plan[plan.indexOf(i[1])] = i[0];
-      // console.log(plan);
     }
   }
   console.log(plan);
