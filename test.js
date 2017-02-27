8fd296f40872bf06fdabed24f25147f5a6f205a0
bednayb <bednaybence@gmail.com>
2017-02-28 00:02:04 +0100

    add more test

--- test.js
+++ test.js
@@ -25,7 +25,6 @@ test('Rules must be array', function (t) {
   t.end();
 })
 
-
 //5. Plan data must be string
 test('Plan data must be string', function (t) {
   t.equal(vacation(["x",7],[]),"Not valid data in plan");
@@ -38,33 +37,59 @@ test("Rule data in 'big' array must be array", function (t) {
   t.end();
 })
 
-//7.
-
-//8. Works with more vacation plan
-test('Works with more vacation plan', function (t) {
-  t.deepEqual(vacation(['x','y','z'],[]),['x','y','z']);
+//7.  every Rule data in 'small' array must have 2 element
+test("every Rule data in 'small' array must have 2 element", function (t) {
+  t.equal(vacation(["x"],[['a'],'s']),"Not valid data in rules");
   t.end();
 })
 
-//9. Vacation plan with one rule
+//8.  every Rule data in 'small' array must be string
+test("every Rule data in 'small' array must be string", function (t) {
+  t.equal(vacation(['x','y','z'],[['x','y'],['y',3]]),"each rule's data should be string");
+  t.end();
+})
+//8.  rules source and termin same
+test("rules source and termin same", function (t) {
+  t.equal(vacation(['x','y','z'],[['x','y'],['y','y']]),"the source point and termin can't be same");
+  t.end();
+})
+//9.  Check every rule's data is a valid destination
+test("every rule's data should be a valid destination", function (t) {
+  t.equal(vacation(['x','y','z'],[['x','y'],['y','a']]),"One or more rule's data isnt a destination");
+  t.end();
+})
+
+//10. Works with more vacation plan
+test('Works with more vacation plan', function (t) {
+  t.deepEqual(vacation(['x','y','z'],[['x','y']]),['x','y','z']);
+  t.end();
+})
+
+//11. Vacation plan with one rule
 test('Vacation plan with one rule', function (t) {
   t.deepEqual(vacation(['x','y','z'],[['y','x']]),['y','x','z']);
   t.end();
 })
 
-//10. Vacation plan with three rules
-test('x with no rules result x', function (t) {
-  t.deepEqual(vacation(['x','y','z'],[['y','x'], ['z','x'],['z','y']]),['z','y','x']);
-  t.end();
-})
-
-//11. Use a destination more than one
+//12. Use a destination more than one
 test('Use a destination more than one', function (t) {
   t.equal(vacation(['x','y','z','x'],[]),"Use every destinations just once");
   t.end();
 })
-//11. Use a rule more than one
+
+//13. Use a rule more than one
 test('Use a rule more than one', function (t) {
   t.equal(vacation(['x','y','z','a'],[['z','x'],['z','x']]),"Use every rule just once");
+  t.end();
+})
+//14. if we have z --> x we can't have x --> z
+test('Use a rule more than one', function (t) {
+  t.equal(vacation(['x','y','z','a'],[['z','x'],['x','z']]),"impossible rules same time");
+  t.end();
+})
+
+//15 Vacation plan with three rules
+test('Vacation plan with three rules', function (t) {
+  t.deepEqual(vacation(['x','y','z'],[['y','x'], ['z','x'],['z','y']]),['z','y','x']);
   t.end();
 })
--- vacation.js
+++ vacation.js
@@ -1,37 +1,55 @@
 'use strict';
 
-// Destinations
-const destinations = ['x','a','z','w'];
 
 // Destinations rules -- in small array contains the rules. the first should be before the second. Example: [['x','y'], ['y','z']]  x --> y  and y --> z
-const rules1 = [['y','x'], ['z','x'],['z','y'],['b','a']];
-const rules2 = [['y','x']];
-const rules3 = [['y','x'],['y','x']];
-const rules4 = 5;
 
 function vacation(plan, rules){
-
   // Check plan is array
   if(!(plan instanceof Array)){
     return "Plan must be array"
-  }
+  };
   // Check every data in the plan (array) is string
   const everyDataIsStringInPlan = plan.every((e)=>{
     return typeof e === 'string'
   })
   if(everyDataIsStringInPlan === false){
     return "Not valid data in plan"
-  }
+  };
   // Check rules is an array
   if(!(rules instanceof Array)){
     return "rules must be array"
-  }
+  };
   // Check every data in 'big array' is array
   const everyDataIsArrayInRules = rules.every((e)=>{
     return e instanceof Array
-  })
+  });
   if(everyDataIsArrayInRules === false){
     return "Not valid data in rules"
+  }
+  let everyRuleData = [];
+  // Check each rules have 2 elements
+  for(let i in rules){
+    if(rules[i].length !== 2){
+      return "each rule should have two elements"
+    }
+    //Check each rule's data is string
+    if( typeof rules[i][0] !== 'string' || typeof rules[i][1] !== 'string'){
+      return "each rule's data should be string"
+    }
+    if( rules[i][0] === rules[i][1]){
+      return "the source point and termin can't be same"
+    }
+    // Check every rule's data is a valid destination
+    everyRuleData.push(rules[i][0]);
+    everyRuleData.push(rules[i][1]);
+  }
+  // Check every elements use just once
+  let uniqueArray = everyRuleData.filter(function(item, pos) {
+    return everyRuleData.indexOf(item) == pos;
+  })
+  const isRulesDataPartOfDestanitaion = uniqueArray.every(function(val) { return plan.indexOf(val) >= 0; });
+  if(isRulesDataPartOfDestanitaion === false){
+    return "One or more rule's data isnt a destination"
   }
   //Check every destinations exists just once
   let checkDestinationsOnce = plan.sort();
@@ -49,8 +67,18 @@ function vacation(plan, rules){
     }
   }
   // Check rules 2. -> example if we have x --> y we can't have y --> z
-  let ellentetesSzabaly = [];
-
+  let copyRules = rules;
+  for(let i = 0; i < copyRules.length; i++){
+  //(with reverse I had problem with reference)
+      let reverse = [];
+      reverse.push(rules[i][1]);
+      reverse.push(rules[i][0]);
+      for(let j = 1; j < copyRules.length; j++){
+        if((JSON.stringify(reverse) === JSON.stringify(copyRules[j])) && ( j > i) && copyRules.length !== 1) {
+          return "impossible rules same time"
+        }
+      }
+    }
   //check rules
   for (let i of rules) {
     if(plan.indexOf(i[0]) > plan.indexOf(i[1])){
@@ -58,10 +86,7 @@ function vacation(plan, rules){
        plan[plan.indexOf(i[1])] = i[0];
     }
   }
-  console.log(plan);
   return plan
 }
-
-vacation(destinations,rules3)
 
 module.exports = vacation;
