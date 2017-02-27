bea5cf8fb20104f4695a46dc5378cb80e6d8d163
bednayb <bednaybence@gmail.com>
2017-02-27 20:32:03 +0100

    add more test and condition

--- test.js
+++ test.js
@@ -1,38 +1,70 @@
 var test = require('tape')
 var vacation = require('./vacation.js')
 
-//TEST WORK
+//1. TEST WORK
 test('true', function (t) {
   t.equal(1, 1);
   t.end();
 });
 
-//Vacation function WORKS
-test('x with no rules result x', function (t) {
+//2. Vacation function WORKS
+test('Vacation function WORKS', function (t) {
   t.deepEqual(vacation(['x'],[]),['x']);
   t.end();
 })
 
-// Works with more vacation plan
-test('x with no rules result x', function (t) {
+//3. Plan must be array
+test('Plan must be array', function (t) {
+  t.deepEqual(vacation("x",[]),"Plan must be array");
+  t.end();
+})
+
+//4. Rules must be array
+test('Rules must be array', function (t) {
+  t.deepEqual(vacation(['x','y','z'],"x"),"rules must be array");
+  t.end();
+})
+
+
+//5. Plan data must be string
+test('Plan data must be string', function (t) {
+  t.equal(vacation(["x",7],[]),"Not valid data in plan");
+  t.end();
+})
+
+//6. every Rule data in 'big' array must be array
+test("Rule data in 'big' array must be array", function (t) {
+  t.equal(vacation(["x"],[['a'],'s']),"Not valid data in rules");
+  t.end();
+})
+
+//7.
+
+//8. Works with more vacation plan
+test('Works with more vacation plan', function (t) {
   t.deepEqual(vacation(['x','y','z'],[]),['x','y','z']);
   t.end();
 })
 
-// Vacation plan with one rule
-test('x with no rules result x', function (t) {
+//9. Vacation plan with one rule
+test('Vacation plan with one rule', function (t) {
   t.deepEqual(vacation(['x','y','z'],[['y','x']]),['y','x','z']);
   t.end();
 })
 
-// Vacation plan with three rules
+//10. Vacation plan with three rules
 test('x with no rules result x', function (t) {
   t.deepEqual(vacation(['x','y','z'],[['y','x'], ['z','x'],['z','y']]),['z','y','x']);
   t.end();
 })
 
-// Use a destination more than one
-test('x with no rules result x', function (t) {
+//11. Use a destination more than one
+test('Use a destination more than one', function (t) {
   t.equal(vacation(['x','y','z','x'],[]),"Use every destinations just once");
+  t.end();
+})
+//11. Use a rule more than one
+test('Use a rule more than one', function (t) {
+  t.equal(vacation(['x','y','z','a'],[['z','x'],['z','x']]),"Use every rule just once");
   t.end();
 })
--- vacation.js
+++ vacation.js
@@ -1,22 +1,56 @@
 'use strict';
 
 // Destinations
-const destinations = ['x','a','z','a','w'];
+const destinations = ['x','a','z','w'];
 
 // Destinations rules -- in small array contains the rules. the first should be before the second. Example: [['x','y'], ['y','z']]  x --> y  and y --> z
 const rules1 = [['y','x'], ['z','x'],['z','y'],['b','a']];
 const rules2 = [['y','x']];
-
+const rules3 = [['y','x'],['y','x']];
+const rules4 = 5;
 
 function vacation(plan, rules){
+
+  // Check plan is array
+  if(!(plan instanceof Array)){
+    return "Plan must be array"
+  }
+  // Check every data in the plan (array) is string
+  const everyDataIsStringInPlan = plan.every((e)=>{
+    return typeof e === 'string'
+  })
+  if(everyDataIsStringInPlan === false){
+    return "Not valid data in plan"
+  }
+  // Check rules is an array
+  if(!(rules instanceof Array)){
+    return "rules must be array"
+  }
+  // Check every data in 'big array' is array
+  const everyDataIsArrayInRules = rules.every((e)=>{
+    return e instanceof Array
+  })
+  if(everyDataIsArrayInRules === false){
+    return "Not valid data in rules"
+  }
   //Check every destinations exists just once
   let checkDestinationsOnce = plan.sort();
-  console.log('pince',checkDestinationsOnce);
   for(let i = 0; i < checkDestinationsOnce.length-1; i++){
     if(checkDestinationsOnce[i] === checkDestinationsOnce[i+1]){
       return "Use every destinations just once"
     }
   }
+  // Check rules 1. -> don't give a rule more than once
+  let checkSameRules = rules.sort();
+  for(let i = 0; i < checkSameRules.length-1; i++){
+    // Check two array is equal
+    if(JSON.stringify(checkSameRules[i]) === JSON.stringify(checkSameRules[i+1])){
+      return "Use every rule just once"
+    }
+  }
+  // Check rules 2. -> example if we have x --> y we can't have y --> z
+  let ellentetesSzabaly = [];
+
   //check rules
   for (let i of rules) {
     if(plan.indexOf(i[0]) > plan.indexOf(i[1])){
@@ -28,6 +62,6 @@ function vacation(plan, rules){
   return plan
 }
 
-vacation(destinations,rules1)
+vacation(destinations,rules3)
 
 module.exports = vacation;
