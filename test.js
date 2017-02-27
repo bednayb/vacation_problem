f2af2e7cfff30c83a4314b1ee74f12ec715a865d
bednayb <bednaybence@gmail.com>
2017-02-27 13:38:00 +0100

    add two new test with rules

--- test.js
+++ test.js
@@ -18,3 +18,15 @@ test('x with no rules result x', function (t) {
   t.deepEqual(vacation(['x','y','z'],[]),['x','y','z']);
   t.end();
 })
+
+// Works with more vacation plan
+test('x with no rules result x', function (t) {
+  t.deepEqual(vacation(['x','y','z'],[['y','x']]),['y','x','z']);
+  t.end();
+})
+
+// Works with more vacation plan
+test('x with no rules result x', function (t) {
+  t.deepEqual(vacation(['x','y','z'],[['y','x'], ['z','x'],['z','y']]),['z','y','x']);
+  t.end();
+})
