1b8e080db5eb3b3f3b3720298cc68ea938fca05c
bednayb <bednaybence@gmail.com>
2017-02-27 10:30:52 +0100

    add basic vacation function

---
+++ vacation.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var array = ['x','y','z'];
+var rules1 = [['x','y'], ['y','z']];
+
+
+function vacation(plan, rules){
+  var result = plan;
+
+  return result
+}
+
+module.exports = vacation;
